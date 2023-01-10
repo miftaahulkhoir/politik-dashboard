3fd2c2df7a92e611254c4bb24d003c3edcffcc36
yosefadi <yosefadi@users.noreply.github.com>
2023-01-10 12:13:30 +0700

    Differentiate between dev and prod env

--- build.sh
+++ build.sh
@@ -3,10 +3,14 @@ export RELEASE_VER=$( git log --grep="Release" --max-count=1 --oneline | awk '{p
 sed -i "/APP_VERSION/c \APP_VERSION=$RELEASE_VER" .env
 if [ -z $1 ] || [ $1 != "master" ]; then
     export DOCKER_IMG_TAG="dev-fe"
+    cp -f .env.development .env
     make cleanimg
     make build
+    rm -rf .env
 else
     export DOCKER_IMG_TAG=$( git log --grep="Release" --max-count=1 --oneline | awk '{tag=substr($3,1,10) "-fe";print tag}' )
+    cp -f .env.production .env
     make build
     make push
+    rm -rf .env
 fi
