a4094b022490d481eef409c691ded4713f5cf831
yosefadi <yosefadi@users.noreply.github.com>
2023-01-11 18:33:07 +0700

    Bypass staging server, straight to production

--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-if [ -z $1 ] || [ $1 != "master" ]; then
-    export DOCKER_IMG_TAG="dev-fe"
-else
-    export DOCKER_IMG_TAG=$( git log --grep="Release" --max-count=1 --oneline | awk '{tag=substr($3,1,10) "-fe";print tag}' )
-fi
+export DOCKER_IMG_TAG=$( git log --grep="Release" --max-count=1 --oneline | awk '{tag=substr($3,1,10) "-fe";print tag}' )
 
 make down
 make run
