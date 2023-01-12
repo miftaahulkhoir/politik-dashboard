df64c90b232819a4230ca618ff20b5aee3227447
yosefadi <yosefadi@users.noreply.github.com>
2023-01-12 09:48:13 +0700

    Update docker image tagging

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export RELEASE_VER=$( git log --grep="Release" --max-count=1 --oneline | awk '{print substr($3,1,10)}' )
-export DOCKER_IMG_TAG=$( git log --grep="Release" --max-count=1 --oneline | awk '{tag=substr($3,1,10) "-fe";print tag}' )
+export RELEASE_VER=$( git log --grep="Release" --max-count=1 --oneline | awk '{print $1}' )
+export DOCKER_IMG_TAG=$( git log --grep="Release" --max-count=1 --oneline | awk '{tag=$1 "-fe";print tag}' )
 echo $DOCKER_IMG_TAG
 cp -f .env.production .env
 sed -i "/APP_VERSION/c \APP_VERSION=$RELEASE_VER" .env
