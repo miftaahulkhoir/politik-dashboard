82d4d36d5c2c6a3d72fdadc4802502448205a4fb
yosefadi <yosefadi@users.noreply.github.com>
2023-01-12 09:43:25 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ export DOCKER_IMG_TAG=$( git log --grep="Release" --max-count=1 --oneline | awk 
 echo $DOCKER_IMG_TAG
 cp -f .env.production .env
 sed -i "/APP_VERSION/c \APP_VERSION=$RELEASE_VER" .env
-make build && make push && rm -rf * && rm -rf .*
+make build && make push && rm -rf *
