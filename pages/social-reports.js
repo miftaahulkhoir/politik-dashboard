d0d561e4583b50067e452b1cae6e5ca13852012e
wahyubrr <wahyu@synapsis.id>
2023-01-13 01:38:35 +0700

    cleaning unused vars

--- pages/social-reports.js
+++ pages/social-reports.js
@@ -1,12 +1,8 @@
-import { notification } from 'antd'
 import axios from "axios";
 import { parseCookies } from "nookies";
 import { useEffect, useState } from "react";
 import Head from 'next/head'
-import DataTable from "react-data-table-component";
 import Card from '../components/elements/card/Card';
-import SummaryCard from '../components/elements/summaryCard/SummaryCard';
-import BlueCard from '../components/pagecomponents/home/BlueCard';
 import SocialChartCard from '../components/pagecomponents/home/SocialChartCard';
 import SocialSummaryCard from '../components/pagecomponents/home/SocialSummaryCard';
 export default function Users(pageProps) {
@@ -19,10 +15,6 @@ export default function Users(pageProps) {
   const [engagementData, setEngagementData] = useState([]);
   const [engagementRateData, setEngagementRateData] = useState([]);
   const [videoViewsData, setVideoViewsData] = useState([]);
-  const [searchList, setSearchList] = useState([]);
-  const [search, setSearch] = useState("");
-
-  const [apiNotification, contextHolderNotification] = notification.useNotification();
 
   useEffect(() => {
     console.log(pageProps.reports);
@@ -39,61 +31,6 @@ export default function Users(pageProps) {
     setEngagementRateData(pageProps.reports.data.engagement_rate);
     setVideoViewsData(pageProps.reports.data.video_views);
   }, []);
-
-  const columns = [
-    {
-      name: "No",
-      selector: (row) => row.no,
-      width: "80px",
-      center: true,
-      sortable: true,
-    },
-    {
-      name: "NIK",
-      width: "80px",
-      selector: (row) => row.NIK,
-    },
-    {
-      name: "Nama",
-      selector: (row) => row?.name,
-    },
-    {
-      name: "Jenis Kelamin",
-      selector: (row) => row?.gender,
-      width: "150px",
-      sortable: true,
-    },
-    {
-      name: "Email",
-      selector: (row) => row?.email,
-      sortable: true,
-    },
-    {
-      name: "Aksi",
-      selector: (row) => "",
-      width: "220px",
-      sortable: true,
-    },
-  ];
-
-  const customStyles = {
-    headCells: {
-      style: {
-        backgroundColor: "#fafafa",
-      },
-    },
-  };
-
-  const handleSearch = (search) => {
-    setSearch(search);
-    let getlist = usersList.filter((x) =>
-      x.name.toLowerCase().includes(search.toLowerCase())
-    );
-    getlist.forEach((element, index) => {
-      getlist[index].no = index + 1;
-    });
-    setSearchList(getlist);
-  };
 
   return (
     <>
