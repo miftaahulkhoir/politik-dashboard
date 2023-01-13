bf5b1084466c052c7cddd18ee94addc54c519a44
yosefadi <yosefadi@users.noreply.github.com>
2023-01-13 10:16:41 +0700

    Update build and run script

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ export DOCKER_IMG_TAG=$( git log --max-count=1 --oneline | awk '{tag=$1 "-fe";pr
 echo $DOCKER_IMG_TAG
 cp -f .env.production .env
 #sed -i "/APP_VERSION/c \APP_VERSION=$RELEASE_VER" .env
-make build && make push && rm -rf *
+make build && make push && find . -name . -o -prune -exec rm -rf -- {} +
--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 export DOCKER_IMG_TAG=$( git log --max-count=1 --oneline | awk '{tag=$1 "-fe";print tag}' )
 
 make down
-make run && rm -rf *
+make run && find . -name . -o -prune -exec rm -rf -- {} +
