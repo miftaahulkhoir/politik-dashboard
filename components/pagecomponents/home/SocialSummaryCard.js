4cd018a47c34f2fcfcc099644af3f4c9e0fec6ae
wahyubrr <wahyu@synapsis.id>
2023-01-18 23:52:14 +0700

    added more charts

--- components/pagecomponents/home/SocialSummaryCard.js
+++ components/pagecomponents/home/SocialSummaryCard.js
@@ -1,7 +1,7 @@
 import NameAvatar from '../../elements/nameAvatar/NameAvatar';
 import styles from './home.module.css';
 
-export default function SocialSummaryCard({ title, subtitle, impressions, engagements, engagementRate, postLinkClicks }) {
+export default function SocialSummaryCard({ title, subtitle, mentionSum, totalImpression, engagementRate, postLinkClicks }) {
   return (
     <div className={styles.social_summary_card}>
       <div className='row'>
@@ -13,12 +13,12 @@ export default function SocialSummaryCard({ title, subtitle, impressions, engage
       <div className={styles.hr}></div>
       <div className="row">
         <div className="col-4">
-          <div className={styles.label}>Impressions</div>
-          <div className={styles.value}>{impressions}</div>
+          <div className={styles.label}>Number of Mentions</div>
+          <div className={styles.value}>{mentionSum}</div>
         </div>
         <div className="col-4">
-          <div className={styles.label}>Engagements</div>
-          <div className={styles.value}>{engagements}</div>
+          <div className={styles.label}>Total Impressions</div>
+          <div className={styles.value}>{totalImpression}</div>
         </div>
         <div className="col-4">
           <div className={styles.label}>Post Link Clicks</div>
