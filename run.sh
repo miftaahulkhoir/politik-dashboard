f034f3739f8e17b3bdb626aa9556d9ecd9dfc8b4
yosefadi <yosefadi@users.noreply.github.com>
2023-01-20 21:48:01 +0700

    Update 3 files

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 source release_ver.sh
 
+rm -rf .env
 if [ -z $1 ] || [ $1 != "master" ]; then
+    export DOCKER_IMG_TAG="dev-fe"
     cp -f .env.development .env
     make down-dev
     make run-dev && find . -name . -o -prune -exec rm -rf -- {} +
