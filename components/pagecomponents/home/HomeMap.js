bbc50a576e645fed50252555eff46f2fd6d5d15e
Muhammad Arlan <arlan.diawan21@gmail.com>
2023-01-21 10:39:56 +0800

    Update Map Log

--- components/pagecomponents/home/HomeMap.js
+++ components/pagecomponents/home/HomeMap.js
@@ -1,13 +1,16 @@
-import 'leaflet/dist/leaflet.css';
-import { useState } from 'react';
+import axios from "axios";
+import "leaflet/dist/leaflet.css";
+import { useEffect, useState } from "react";
 import {
   MapContainer,
   Marker,
+  Polyline,
   TileLayer,
   Tooltip,
+  useMap,
   useMapEvents,
-} from 'react-leaflet';
-import styles from '../../elements/map/Home.module.css';
+} from "react-leaflet";
+import styles from "../../elements/map/Home.module.css";
 
 export default function HomeMap({
   showKoordinator,
@@ -18,112 +21,206 @@ export default function HomeMap({
   dataPemilih,
   showBlackList,
   dataBlackList,
+  center,
+  setCenter,
+  tempCenter,
+  setTempCenter,
+  userLogCordinate,
+  setUserLogCordinate,
+  logCordinate,
+  setLogCordinate,
+  recenter,
+  setRecenter,
   handleColor,
 }) {
   const [zoom, setZoom] = useState(11);
-  const [center, setCenter] = useState({ lat: -7.0335559, lng: 107.6589375 });
   const [iconSize, setIconSize] = useState(30);
+  // const [polygonCordinate, setpolygonCordinate] = useState([]);
+  const [logType, setLogType] = useState("");
+  const [map, setMap] = useState(null);
+
+  const handleDetailCordinate = (userid, name, type) => {
+    setLogType(type);
+    axios
+      .get(`${process.env.APP_BASEURL}api/data-mapping?userid=${userid}`)
+      .then((res) => {
+        res.data.data.forEach((element, index) => {
+          let arr = [];
+          axios
+            .get(
+              `https://api.geoapify.com/v1/geocode/reverse?lat=${element?.latitude}&lon=${element?.longitude}&apiKey=5523a1bf84d64e849bdd9a6ca7af26e2`
+            )
+            .then((res2) => {
+              setLogCordinate((prev) => [
+                ...prev,
+                {
+                  name: name,
+                  locationName: `${res2.data.features[0]?.properties?.address_line1}, ${res2.data.features[0]?.properties?.address_line2}`,
+                  ...element,
+                },
+              ]);
+            })
+            .catch((err) => {});
+          // setpolygonCordinate((prev) => [
+          //   ...prev,
+          //   [element.latitude, element.longitude],
+          // ]);
+        });
+        map.flyTo([
+          res.data.data[res.data.data.length - 1]?.latitude,
+          res.data.data[res.data.data.length - 1]?.longitude,
+        ]);
+      })
+      .catch((err) => {});
+  };
 
   return (
     <MapContainer
       center={center}
       zoom={zoom}
       zoomControl={false}
-      className={styles.homeMap}
-    >
+      ref={setMap}
+      className={styles.homeMap}>
       <HomeMapComponent
         setZoom={setZoom}
+        recenter={recenter}
+        tempCenter={tempCenter}
         setCenter={setCenter}
+        setRecenter={setRecenter}
         setIconSize={setIconSize}
       />
       <TileLayer
-        className="map"
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        className='map'
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         attribution='&copy; <a href="http://osorg/copyright">OpenStreetMap</a> contributors'
       />
       {showKoordinator === true &&
+        userLogCordinate === false &&
         dataKoordinator.map((m, index) => (
           <Marker
             key={index}
+            eventHandlers={{
+              click: (e) => {
+                handleDetailCordinate(m.id, m.name, "koordinator"),
+                  setTempCenter([m.latitude, m.longitude]),
+                  setUserLogCordinate(true);
+              },
+            }}
             icon={
               new L.Icon({
-                iconUrl: '/images/map/markers/user-koordinator.svg',
+                iconUrl: "/images/map/markers/user-koordinator.svg",
                 iconSize: [iconSize, iconSize],
                 iconAnchor: [iconSize / 2, iconSize / 2],
               })
             }
-            position={[m?.latitude, m?.longitude]}
-          >
-            <Tooltip direction="top" offset={[0, -10]} opacity={1} permanent>
+            position={[m?.latitude, m?.longitude]}>
+            <Tooltip direction='top' offset={[0, -10]} opacity={1} permanent>
               {m.name}
             </Tooltip>
           </Marker>
         ))}
       {showRelawan === true &&
+        userLogCordinate === false &&
         dataRelawan.map(
           (m, index) =>
-            m.longitude !== '' && (
+            m.longitude !== "" && (
               <Marker
                 key={index}
+                eventHandlers={{
+                  click: (e) => {
+                    handleDetailCordinate(m.id, m.name, "relawan"),
+                      setTempCenter([m.latitude, m.longitude]),
+                      setUserLogCordinate(true);
+                  },
+                }}
                 icon={
                   new L.Icon({
-                    iconUrl: '/images/map/markers/user-relawan.svg',
+                    iconUrl: "/images/map/markers/user-relawan.svg",
                     iconSize: [iconSize, iconSize],
                     iconAnchor: [iconSize / 2, iconSize / 2],
                   })
                 }
-                position={[m?.latitude, m?.longitude]}
-              >
+                position={[m?.latitude, m?.longitude]}>
                 <Tooltip
-                  direction="top"
+                  direction='top'
                   offset={[0, -10]}
                   opacity={1}
-                  permanent
-                >
+                  permanent>
                   {m.name}
                 </Tooltip>
               </Marker>
             )
         )}
       {showPemilih === true &&
+        userLogCordinate === false &&
         dataPemilih.map(
           (m, index) =>
-            m.longitude !== '' && (
+            m.longitude !== "" && (
               <Marker
                 key={index}
                 icon={
                   new L.Icon({
-                    iconUrl: '/images/map/markers/user-pemilih.svg',
+                    iconUrl: "/images/map/markers/user-pemilih.svg",
                     iconSize: [iconSize, iconSize],
                     iconAnchor: [iconSize / 2, iconSize / 2],
                   })
                 }
-                position={[m?.latitude, m?.longitude]}
-              ></Marker>
+                position={[m?.latitude, m?.longitude]}></Marker>
             )
         )}
       {showBlackList === true &&
+        userLogCordinate === false &&
         dataBlackList.map(
           (m, index) =>
-            m.longitude !== '' && (
+            m.longitude !== "" && (
               <Marker
                 key={index}
                 icon={
                   new L.Icon({
-                    iconUrl: '/images/map/markers/user-blacklist.svg',
+                    iconUrl: "/images/map/markers/user-blacklist.svg",
                     iconSize: [iconSize, iconSize],
                     iconAnchor: [iconSize / 2, iconSize / 2],
                   })
                 }
-                position={[m?.latitude, m?.longitude]}
-              ></Marker>
+                position={[m?.latitude, m?.longitude]}></Marker>
             )
         )}
+      {userLogCordinate === true && (
+        <>
+          {logCordinate.map((m, index) => (
+            <Marker
+              key={index}
+              icon={
+                new L.Icon({
+                  iconUrl: `/images/map/markers/user-${logType}.svg`,
+                  iconSize: [iconSize, iconSize],
+                  iconAnchor: [iconSize / 2, iconSize / 2],
+                })
+              }
+              position={[m?.latitude, m?.longitude]}>
+              <Tooltip direction='top' offset={[0, -10]} opacity={1} permanent>
+                {m.name}
+              </Tooltip>
+            </Marker>
+          ))}
+          {/* <Polyline
+            pathOptions={{ color: "red" }}
+            positions={polygonCordinate}
+          /> */}
+        </>
+      )}
     </MapContainer>
   );
 }
 
-function HomeMapComponent({ setZoom, setCenter, setIconSize }) {
+function HomeMapComponent({
+  setZoom,
+  recenter,
+  tempCenter,
+  setRecenter,
+  setCenter,
+  setIconSize,
+}) {
   const scaleZoom = (input) => {
     return input / 19;
   };
@@ -131,14 +228,19 @@ function HomeMapComponent({ setZoom, setCenter, setIconSize }) {
   const mapEvents = useMapEvents({
     zoomend: () => {
       const zoom = mapEvents.getZoom();
-      setZoom(zoom);
-      setCenter(mapEvents.getCenter());
-
+      // setZoom(zoom);
+      // setCenter(mapEvents.getCenter());
       setIconSize(40 * scaleZoom(zoom) + 1);
     },
-    dragend: () => {
-      setCenter(mapEvents.getCenter());
-    },
+    // dragend: () => {
+    //   setCenter(mapEvents.getCenter());
+    // },
   });
+
+  if (recenter === true) {
+    mapEvents.setZoom(11).flyTo([tempCenter[0], tempCenter[1]]);
+    setRecenter(false);
+  }
+
   return null;
 }
