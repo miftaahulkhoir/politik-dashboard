7224d4d2b48431ec759010644b9c1cbbb1186f1a
Lutfi Andriyanto <lutfiandri232@gmail.com>
2023-02-06 09:05:49 +0700

    feat(surveys): location result

--- components/pagecomponents/surveys/SurveyResponseDrawer2.js
+++ components/pagecomponents/surveys/SurveyResponseDrawer2.js
@@ -18,14 +18,20 @@ export default function SurveyResponseDrawer2({ open, setOpen, selectedResponse 
         const answers = anss.map((ans) => {
           return {
             id: ans.id,
-            question: ans.question.question_name,
-            answer: ans.answer,
+            question: ans?.question?.question_name,
+            answer: ans?.answer,
+            questionType: ans?.question?.input_type,
           };
         });
         setAnswers([...answers]);
       })
       .catch((err) => {});
   }, [selectedResponse, open]);
+
+  const answerText = (answer) => {
+    if (answer.questionType === "location") return answer.answer.reverse().join(", ");
+    return answer.answer;
+  };
 
   return (
     <Drawer
@@ -40,7 +46,7 @@ export default function SurveyResponseDrawer2({ open, setOpen, selectedResponse 
         {answers.map((ans) => (
           <div key={ans.id}>
             <div>{ans.question}</div>
-            <div className={styles.ans_answer}>{ans.answer}</div>
+            <div className={styles.ans_answer}>{answerText(ans)}</div>
           </div>
         ))}
       </Space>
