c87fb113e29583e1e5ee8f65b9d1207e06e4c6ba
wahyubrr <wahyu@synapsis.id>
2023-02-22 17:38:52 +0700

    cleaning up home navbar

--- components/pagecomponents/home/HomeNavbar.js
+++ components/pagecomponents/home/HomeNavbar.js
@@ -15,8 +15,6 @@ export default function HomeNavbar({ smallDevice, setActive }) {
     { name: "Manajemen Survei", path: "/surveys" },
     { name: "Analisis Survei", path: "/survey-analysis" },
     { name: "social-media" },
-    { name: "Analisis Sentimen", path: "/sentiment-analysis" },
-    { name: "Panel Ads", path: "/panel-ads" },
     { name: "WhatsApp Blast", path: "/whatsapp-blast" },
   ];
 
