ecff308dedb2e69861c7ddedf92e73c9b590a41a
Muhammad Adnan R <madnanrizqullah@gmail.com>
2023-03-10 06:58:57 +0700

    feat(map): improve region detail drawer with multiple charts

--- components/pagecomponents/home/drawer/RegionQuestionDetailDrawer.js
+++ components/pagecomponents/home/drawer/RegionQuestionDetailDrawer.js
@@ -1,8 +1,69 @@
-import { Drawer, Grid, Space } from "antd";
+import { Drawer, Grid, Space, Tabs } from "antd";
 import React, { useMemo } from "react";
 
 import SurveyHorizontalBarChart from "../../surveyAnalitics/charts/SurveyHorizontalBarChart";
+import SurveyPieChart from "../../surveyAnalitics/charts/SurveyPieChart";
+import SurveyTableChart from "../../surveyAnalitics/charts/SurveyTableChart";
 
+function RegionQuestionDetail({ questionResponse: response, selectedRegionLevel, selectedRegion }) {
+  const getPieData = (options, counts) => {
+    const result = counts?.map((count, index) => ({
+      name: options[index],
+      value: count ?? 0,
+    }));
+    return result;
+  };
+
+  const getTableData = (options, counts) => {
+    const result = counts?.map((count, index) => ({
+      option_name: options[index],
+      total_answer: count ?? 0,
+    }));
+    return result;
+  };
+
+  const tableData = (response) => {
+    if (selectedRegionLevel == 2) {
+      return getTableData(response?.options, response?.district_counts);
+    }
+    return getTableData(response?.options, response?.counts);
+  };
+
+  const pieData = (response) => {
+    if (selectedRegionLevel == 2) {
+      return getPieData(response?.options, response?.district_counts);
+    }
+    return getPieData(response?.options, response?.counts);
+  };
+
+  const tabItems = [
+    {
+      key: "1",
+      label: `Bar Chart`,
+      children: (
+        <SurveyHorizontalBarChart
+          title={response?.question}
+          dataX={pieData(response).map((d) => d.value)}
+          dataY={pieData(response).map((d) => d.name)}
+          colors={response?.colors}
+          fitTitleHeight
+        />
+      ),
+    },
+    {
+      key: "2",
+      label: `Table Chart`,
+      children: <SurveyTableChart title={response?.question} options={tableData(response)} />,
+    },
+    {
+      key: "3",
+      label: `Pie Chart`,
+      children: <SurveyPieChart title={response?.question} data={pieData(response)} />,
+    },
+  ];
+
+  return <Tabs defaultActiveKey="1" items={tabItems} />;
+}
 export default function RegionQuestionDetailDrawer({ open, setOpen, selectedRegion, selectedRegionLevel }) {
   const screen = Grid.useBreakpoint();
 
@@ -12,21 +73,6 @@ export default function RegionQuestionDetailDrawer({ open, setOpen, selectedRegi
 
   const onClose = () => {
     setOpen(false);
-  };
-
-  const getPieData = (options, counts) => {
-    const result = counts?.map((count, index) => ({
-      name: options[index],
-      value: count ?? 0,
-    }));
-    return result;
-  };
-
-  const pieData = (response) => {
-    if (selectedRegionLevel == 2) {
-      return getPieData(response?.options, response?.district_counts);
-    }
-    return getPieData(response?.options, response?.counts);
   };
 
   const title = useMemo(() => {
@@ -50,24 +96,18 @@ export default function RegionQuestionDetailDrawer({ open, setOpen, selectedRegi
       <Space direction="vertical" size="small" key={selectedRegion?.village_id}>
         {selectedRegion?.question_responses?.length == 0 || !selectedRegion?.question_responses ? (
           <div>Tidak ada data yang tersedia di daerah ini</div>
-        ) : null}
-        {selectedRegion?.question_responses?.map((response) => {
-          return (
-            // <SurveyPieChart
-            //   key={response?.question + selectedRegion?.village_id}
-            //   title={response?.question}
-            //   data={pieData(response)}
-            // />
-            <SurveyHorizontalBarChart
-              key={response?.question + selectedRegion?.village_id}
-              title={response?.question}
-              dataX={pieData(response).map((d) => d.value)}
-              dataY={pieData(response).map((d) => d.name)}
-              colors={response?.colors}
-              fitTitleHeight
-            />
-          );
-        })}
+        ) : (
+          selectedRegion?.question_responses?.map((response) => {
+            return (
+              <RegionQuestionDetail
+                key={response?.question + selectedRegion?.village_id}
+                questionResponse={response}
+                selectedRegion={selectedRegion}
+                selectedRegionLevel={selectedRegionLevel}
+              />
+            );
+          })
+        )}
       </Space>
     </Drawer>
   );
