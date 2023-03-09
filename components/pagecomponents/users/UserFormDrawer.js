41b65b3d597225ffadd8e1734ac61550b89156a3
Muhammad Adnan R <madnanrizqullah@gmail.com>
2023-03-10 06:24:08 +0700

    fix(users): add validation to name input

--- components/pagecomponents/users/UserFormDrawer.js
+++ components/pagecomponents/users/UserFormDrawer.js
@@ -1,4 +1,4 @@
-import { Button, Col, Drawer, Grid, Input, Radio, Row, Select, Typography } from "antd";
+import { Button, Col, Drawer, Form, Grid, Input, Radio, Row, Select, Typography } from "antd";
 import React, { useEffect, useMemo, useState } from "react";
 
 import { useFindAllDistrictsByRegencyID, useFindAllRegencies } from "../../../utils/services/locations";
@@ -155,103 +155,111 @@ export default function UserFormDrawer({
       width={isSM ? "100%" : "500px"}
       headerStyle={{ border: "none", fontSize: "32px" }}
     >
-      <Row>
-        <Col span={24} style={{ marginBottom: "24px" }}>
-          <Typography.Title level={5}>Nama Lengkap</Typography.Title>
-          <Input value={name} onChange={(e) => setName(e.target.value)} />
-        </Col>
-
-        <Col span={24} style={{ marginBottom: "24px" }}>
-          <Typography.Title level={5}>Jenis Kelamin</Typography.Title>
-          <Radio.Group
-            disabled={isEdit}
-            value={gender}
-            onChange={(e) => {
-              setGender(e.target.value);
-            }}
-            optionType="button"
-            buttonStyle="solid"
-            options={[
-              { label: "Laki-laki", value: "male" },
-              { label: "Perempuan", value: "female" },
-            ]}
-          ></Radio.Group>
-        </Col>
-        <Col span={24} style={{ marginBottom: "24px" }}>
-          <Typography.Title level={5}>NIK</Typography.Title>
-          <Input value={nik} disabled={isEdit} onChange={(e) => setNik(e.target.value)} />
-        </Col>
-        <Col span={24} style={{ marginBottom: "24px" }}>
-          <Typography.Title level={5}>Nomor WhatsApp</Typography.Title>
-          <Input value={wa} disabled={isEdit} onChange={(e) => setWa(e.target.value)} />
-        </Col>
-        <Col span={24} style={{ marginBottom: "24px" }}>
-          <Typography.Title level={5}>Email</Typography.Title>
-          <Input value={email} disabled={isEdit} onChange={(e) => setEmail(e.target.value)} />
-        </Col>
-        {!isEdit ? (
+      <Form>
+        <Row>
           <Col span={24} style={{ marginBottom: "24px" }}>
-            <Typography.Title level={5}>Password</Typography.Title>
-            <Input.Password value={password} disabled={isEdit} onChange={(e) => setPassword(e.target.value)} />
+            <Typography.Title level={5}>Nama Lengkap</Typography.Title>
+            <Form.Item
+              name="name"
+              rules={[{ max: 2, message: "Name maksimal 50 karakter" }]}
+              style={{ marginBottom: 0 }}
+            >
+              <Input value={name} onChange={(e) => setName(e.target.value)} />
+            </Form.Item>
           </Col>
-        ) : null}
-        <Col span={24} style={{ marginBottom: "24px" }}>
-          <Typography.Title level={5}>Jabatan</Typography.Title>
-          <Select
-            showSearch
-            placeholder="Pilih Role"
-            value={occupation || undefined}
-            disabled={isEdit}
-            onChange={(value) => setOccupation(value)}
-            style={{ width: "100%" }}
-            filterOption={(input, option) => (option?.label ?? "").toLowerCase().includes(input.toLowerCase())}
-            options={occupations
-              // .filter((o) => o.level > currentUser?.occupation?.level)
-              .filter((o) => o.level === currentUser?.occupation?.level + 1)
-              .map((o) => ({ label: o.name, value: o.id }))}
-          />
-        </Col>
-        <Col span={24} style={{ marginBottom: "24px" }}>
-          <Typography.Title level={5}>Kabupaten</Typography.Title>
-          <Select
-            showSearch
-            placeholder="Pilih Kota/Kabupaten"
-            value={regency || undefined}
-            disabled={isEdit}
-            onChange={(value) => setRegency(value)}
-            style={{ width: "100%" }}
-            filterOption={(input, option) => (option?.label ?? "").toLowerCase().includes(input.toLowerCase())}
-            options={regencies.map((r) => ({ label: r.name, value: r.id }))}
-          />
-        </Col>
-        <Col span={24} style={{ marginBottom: "24px" }}>
-          <Typography.Title level={5}>Kecamatan</Typography.Title>
-          <Select
-            showSearch
-            placeholder="Pilih Kecamatan"
-            value={district || undefined}
-            disabled={isEdit || !regency}
-            onChange={(value) => setDistrict(value)}
-            style={{ width: "100%" }}
-            filterOption={(input, option) => (option?.label ?? "").toLowerCase().includes(input.toLowerCase())}
-            options={districts.map((d) => ({ label: d.name, value: d.id }))}
-          />
-        </Col>
-        <Col span={24} style={{ marginBottom: "24px" }}>
-          <Typography.Title level={5}>Lokasi (Latitude)</Typography.Title>
-          <Input value={latitude} disabled={isEdit} onChange={(e) => setLatitude(e.target.value)} />
-        </Col>
-        <Col span={24} style={{ marginBottom: "24px" }}>
-          <Typography.Title level={5}>Lokasi (Longitude)</Typography.Title>
-          <Input value={longitude} disabled={isEdit} onChange={(e) => setLongitude(e.target.value)} />
-        </Col>
 
-        <div style={{ display: "flex", justifyContent: "end", width: "100%" }}>
-          <Button type="primary" onClick={submitHandler} style={{ fontWeight: 600, letterSpacing: "0.8px" }}>
-            SIMPAN
-          </Button>
-        </div>
-      </Row>
+          <Col span={24} style={{ marginBottom: "24px" }}>
+            <Typography.Title level={5}>Jenis Kelamin</Typography.Title>
+            <Radio.Group
+              disabled={isEdit}
+              value={gender}
+              onChange={(e) => {
+                setGender(e.target.value);
+              }}
+              optionType="button"
+              buttonStyle="solid"
+              options={[
+                { label: "Laki-laki", value: "male" },
+                { label: "Perempuan", value: "female" },
+              ]}
+            ></Radio.Group>
+          </Col>
+          <Col span={24} style={{ marginBottom: "24px" }}>
+            <Typography.Title level={5}>NIK</Typography.Title>
+            <Input value={nik} disabled={isEdit} onChange={(e) => setNik(e.target.value)} />
+          </Col>
+          <Col span={24} style={{ marginBottom: "24px" }}>
+            <Typography.Title level={5}>Nomor WhatsApp</Typography.Title>
+            <Input value={wa} disabled={isEdit} onChange={(e) => setWa(e.target.value)} />
+          </Col>
+          <Col span={24} style={{ marginBottom: "24px" }}>
+            <Typography.Title level={5}>Email</Typography.Title>
+            <Input value={email} disabled={isEdit} onChange={(e) => setEmail(e.target.value)} />
+          </Col>
+          {!isEdit ? (
+            <Col span={24} style={{ marginBottom: "24px" }}>
+              <Typography.Title level={5}>Password</Typography.Title>
+              <Input.Password value={password} disabled={isEdit} onChange={(e) => setPassword(e.target.value)} />
+            </Col>
+          ) : null}
+          <Col span={24} style={{ marginBottom: "24px" }}>
+            <Typography.Title level={5}>Jabatan</Typography.Title>
+            <Select
+              showSearch
+              placeholder="Pilih Role"
+              value={occupation || undefined}
+              disabled={isEdit}
+              onChange={(value) => setOccupation(value)}
+              style={{ width: "100%" }}
+              filterOption={(input, option) => (option?.label ?? "").toLowerCase().includes(input.toLowerCase())}
+              options={occupations
+                // .filter((o) => o.level > currentUser?.occupation?.level)
+                .filter((o) => o.level === currentUser?.occupation?.level + 1)
+                .map((o) => ({ label: o.name, value: o.id }))}
+            />
+          </Col>
+          <Col span={24} style={{ marginBottom: "24px" }}>
+            <Typography.Title level={5}>Kabupaten</Typography.Title>
+            <Select
+              showSearch
+              placeholder="Pilih Kota/Kabupaten"
+              value={regency || undefined}
+              disabled={isEdit}
+              onChange={(value) => setRegency(value)}
+              style={{ width: "100%" }}
+              filterOption={(input, option) => (option?.label ?? "").toLowerCase().includes(input.toLowerCase())}
+              options={regencies.map((r) => ({ label: r.name, value: r.id }))}
+            />
+          </Col>
+          <Col span={24} style={{ marginBottom: "24px" }}>
+            <Typography.Title level={5}>Kecamatan</Typography.Title>
+            <Select
+              showSearch
+              placeholder="Pilih Kecamatan"
+              value={district || undefined}
+              disabled={isEdit || !regency}
+              onChange={(value) => setDistrict(value)}
+              style={{ width: "100%" }}
+              filterOption={(input, option) => (option?.label ?? "").toLowerCase().includes(input.toLowerCase())}
+              options={districts.map((d) => ({ label: d.name, value: d.id }))}
+            />
+          </Col>
+          <Col span={24} style={{ marginBottom: "24px" }}>
+            <Typography.Title level={5}>Lokasi (Latitude)</Typography.Title>
+            <Input value={latitude} disabled={isEdit} onChange={(e) => setLatitude(e.target.value)} />
+          </Col>
+          <Col span={24} style={{ marginBottom: "24px" }}>
+            <Typography.Title level={5}>Lokasi (Longitude)</Typography.Title>
+            <Input value={longitude} disabled={isEdit} onChange={(e) => setLongitude(e.target.value)} />
+          </Col>
+
+          <div style={{ display: "flex", justifyContent: "end", width: "100%" }}>
+            <Button type="primary" onClick={submitHandler} style={{ fontWeight: 600, letterSpacing: "0.8px" }}>
+              SIMPAN
+            </Button>
+          </div>
+        </Row>
+      </Form>
     </Drawer>
   );
 }
