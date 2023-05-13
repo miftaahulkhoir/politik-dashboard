86227218b06a5ceebe10fdaa7b03ba90e77fa3bc
Miftahul Khoir <miftahulkhoir@Miftahuls-MacBook-Pro.local>
2023-05-13 22:40:39 +0700

    feat: slicing analisis layer

--- components/pagecomponents/home/Menu.jsx
+++ components/pagecomponents/home/Menu.jsx
@@ -2,15 +2,13 @@ import React, { Fragment, useEffect, useState } from "react";
 
 import Sidebar from "./Sidebar";
 import TopBar from "./TopBar";
-import { MdOutlineLayers } from "react-icons/md";
+import { MdOutlineLayers, MdShowChart, MdOpenInNew } from "react-icons/md";
 import { BsCaretDownFill, BsChevronLeft, BsChevronRight } from "react-icons/bs";
 import moment from "moment";
 import { Listbox, Transition } from "@headlessui/react";
 import { useFindAllIssues, useFindAllYearsByIssue, useFindAllSubIssues } from "@/utils/services/issue";
 
-const year = [{ name: "2019" }, { name: "2020" }, { name: "2021" }];
-
-const LayerFilter = () => {
+const LayerFilter = ({ setIsShowGeoJSON }) => {
   const { issues, isLoading } = useFindAllIssues();
   const [isLayerOpen, setIsLayerOpen] = useState(false);
 
@@ -29,6 +27,12 @@ const LayerFilter = () => {
     if (selectedYear || isLoadingYear || !years.length) return;
     setSelectedYear(years[years.length - 1]);
   }, [isLoadingYear]);
+
+  useEffect(() => {
+    if (selected && selectedYear && isLayerOpen) {
+      setIsShowGeoJSON(true);
+    }
+  }, [selected, selectedYear, isLayerOpen]);
 
   const mappingIssues = (Boolean(issues.length) && Object.fromEntries(issues.map((obj) => [obj.value, obj]))) || [];
   const mappingYears = (Boolean(years.length) && Object.fromEntries(years.map((obj) => [obj.value, obj]))) || [];
@@ -224,12 +228,81 @@ const LayerFilter = () => {
   );
 };
 
-const Menu = () => {
+const LayerData = () => {
+  return (
+    <div className="absolute right-0 top-[78px] text-white">
+      <div className="flex items-center bg-new-black px-[24px] h-[78px]">
+        <MdShowChart size={32} />
+        <span className="font-semibold text-xl">Analisis</span>
+      </div>
+      <div className="flex flex-col justify-between p-8 w-[329px] h-[calc(100vh-150px)] bg-new-black-secondary gap-3">
+        <div className="flex flex-col gap-3">
+          <span className="font-bold text-sm">Provinsi</span>
+          <div className="flex flex-col text-sm gap-3">
+            <div className="flex justify-between">
+              <span>Sumatra Utara</span>
+              <div className="flex items-center gap-1.5">
+                <span>16k</span> <MdOpenInNew />
+              </div>
+            </div>
+            <div className="flex justify-between">
+              <span>Metro Jaya</span>
+              <div className="flex items-center gap-1.5">
+                <span>12k</span> <MdOpenInNew />
+              </div>
+            </div>
+            <div className="flex justify-between">
+              <span>Jawa Timur</span>
+              <div className="flex items-center gap-1.5">
+                <span>8k</span> <MdOpenInNew />
+              </div>
+            </div>
+            <div className="flex justify-between">
+              <span>Sulawesi Selatan</span>
+              <div className="flex items-center gap-1.5">
+                <span>4k</span> <MdOpenInNew />
+              </div>
+            </div>
+            <div className="flex justify-between">
+              <span>Sumatera Selatan</span>
+              <div className="flex items-center gap-1.5">
+                <span>2k</span> <MdOpenInNew />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 border-t py-4">
+          <span className="font-bold text-sm">Legenda</span>
+          <div>
+            <div className="flex gap-2.5 items-center">
+              <div className="w-5 h-5 bg-[#F78A25]"></div>
+              <span className="text-sm">{">10k"}</span>
+            </div>
+            <div className="flex gap-2.5 items-center">
+              <div className="w-5 h-5 bg-[#F8AC66]"></div>
+              <span className="text-sm">8k - 10k</span>
+            </div>
+            <div className="flex gap-2.5 items-center">
+              <div className="w-5 h-5 bg-[#FDC694]"></div>
+              <span className="text-sm">4k - 8k</span>
+            </div>
+            <div className="flex gap-2.5 items-center">
+              <div className="w-5 h-5 bg-[#FFE3CA]"></div>
+              <span className="text-sm">{"< 4k"}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+const Menu = ({ setIsShowGeoJSON }) => {
   return (
     <>
       <Sidebar />
-      <LayerFilter />
+      <LayerFilter setIsShowGeoJSON={setIsShowGeoJSON} />
       <TopBar />
+      <LayerData />
     </>
   );
 };
--- containers/patroli/index.jsx
+++ containers/patroli/index.jsx
@@ -15,6 +15,7 @@ const PatroliPage = ({ profile }) => {
   const [isMounted, setIsMounted] = useState(false);
   // eslint-disable-next-line no-loss-of-precision
   const [cordinate] = useState([-2.0459326720699523, 122.07302997496033]);
+  const [isShowGeoJSON, setIsShowGeoJSON] = useState(false);
 
   useEffect(() => {
     setIsMounted(true);
@@ -48,14 +49,16 @@ const PatroliPage = ({ profile }) => {
                       url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
                       attribution='&copy; <a href="http://osorg/copyright">OpenStreetMap</a> contributors'
                     />
-                    <GeoJSON ref={GeoJSONEL} attribution="&copy; credits due..." data={geojson} style={style} />
+                    {isShowGeoJSON && (
+                      <GeoJSON ref={GeoJSONEL} attribution="&copy; credits due..." data={geojson} style={style} />
+                    )}
                   </>
                 );
               }}
             </Map>
           </div>
         )}
-        <Menu />
+        <Menu setIsShowGeoJSON={setIsShowGeoJSON} />
       </div>
     </>
   );
