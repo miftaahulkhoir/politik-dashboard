263d1fbdcad1dbb3a11239c3920b2e5d0eccaaa8
Teuku Rijal <tm.rijalul@mifx.com>
2023-05-31 16:36:14 +0700

    fix: add advance feature image talkwalker

--- pages/talkwalker/index.jsx
+++ pages/talkwalker/index.jsx
@@ -1,29 +1,83 @@
 import DashboardLayout from "@/layouts/DashboardLayout";
+import { Divider } from "antd";
 import { useState } from "react";
+import { ImConfused2 } from "react-icons/im";
+import { MdFeaturedVideo } from "react-icons/md";
 export const talkwalkerData = [
   {
     id: 1,
+    label: <MdFeaturedVideo color="black" />,
     img: "bg-[url(/images/talkwalker/menu1.png)]",
   },
   {
     id: 2,
+    label: <MdFeaturedVideo color="black" />,
     img: "bg-[url(/images/talkwalker/menu2.png)]",
   },
   {
     id: 3,
+    label: <MdFeaturedVideo color="black" />,
     img: "bg-[url(/images/talkwalker/menu3.png)]",
   },
   {
     id: 4,
+    label: <MdFeaturedVideo color="black" />,
     img: "bg-[url(/images/talkwalker/menu4.png)]",
   },
   {
     id: 5,
+    label: <MdFeaturedVideo color="black" />,
     img: "bg-[url(/images/talkwalker/menu5.png)]",
   },
   {
     id: 6,
+    label: <MdFeaturedVideo color="black" />,
     img: "bg-[url(/images/talkwalker/menu6.png)]",
+  },
+  {
+    id: 7,
+    label: <MdFeaturedVideo color="black" />,
+    img: "bg-[url(/images/talkwalker/menu7.png)]",
+  },
+  {
+    id: 8,
+    label: <MdFeaturedVideo color="black" />,
+    img: "bg-[url(/images/talkwalker/menu8.png)]",
+  },
+  {
+    id: 9,
+    label: <MdFeaturedVideo color="black" />,
+    img: "bg-[url(/images/talkwalker/menu9.png)]",
+  },
+  {
+    id: 10,
+    label: <MdFeaturedVideo color="black" />,
+    img: "bg-[url(/images/talkwalker/menu10.png)]",
+  },
+  {
+    id: 11,
+    label: <MdFeaturedVideo color="black" />,
+    img: "bg-[url(/images/talkwalker/menu11.png)]",
+  },
+  {
+    id: 12,
+    label: <MdFeaturedVideo color="black" />,
+    img: "bg-[url(/images/talkwalker/menu12.png)]",
+  },
+  {
+    id: 13,
+    label: <MdFeaturedVideo color="black" />,
+    img: "bg-[url(/images/talkwalker/menu13.png)]",
+  },
+  {
+    id: 14,
+    label: <MdFeaturedVideo color="black" />,
+    img: "bg-[url(/images/talkwalker/menu14.png)]",
+  },
+  {
+    id: 15,
+    label: <MdFeaturedVideo color="black" />,
+    img: "bg-[url(/images/talkwalker/menu15.png)]",
   },
 ];
 
@@ -32,15 +86,18 @@ export default function TalkwalkerPage(props) {
 
   return (
     <DashboardLayout topBarConfig={{ isShowSearchRegion: true, title: "Analisis Sosial", hideMapButton: true }}>
-      <div className="flex flex-row mt-14 ml-[62px] bg-[#222222] h-[calc(100vh-134px)] overflow-auto text-white relative ">
-        <div className="bg-transparent h-full w-[65px] z-10 pt-[100px] flex-row">
-          {talkwalkerData.map((item) => (
-            <div key={item.key} className="p-5 decoration-black cursor-pointer" onClick={() => setSelectedMenu(item)}>
-              {" "}
-            </div>
+      <div className="flex flex-row mt-14 ml-[62px] bg-[#FFFFFFF] h-[calc(100vh-134px)] overflow-auto text-white relative">
+        <div className="h-full w-[65px] z-10 flex-row border-r-2">
+          {talkwalkerData.map((item, index) => (
+            <>
+              {index === 6 && <div className="border-t-2" />}
+              <div key={item.key} className="p-4 decoration-black cursor-pointer" onClick={() => setSelectedMenu(item)}>
+                {item.label}
+              </div>
+            </>
           ))}
         </div>
-        <div className={`${selectedMenu?.img} bg-cover bg-no-repeat w-full h-full absolute`} />
+        <div className={`${selectedMenu?.img} bg-cover bg-no-repeat w-full h-full`} />
       </div>
     </DashboardLayout>
   );
